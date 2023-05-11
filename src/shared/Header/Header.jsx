940b700c6723a0271ec55355e10bbc97b8752d85
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-05-12 01:58:55 +0600

    first commit

---
+++ src/shared/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from '../../assets/logo.svg'
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+
+    const navigation = [
+        <li><Link to="/home">Home</Link></li>,
+        <li><Link to="/about">About</Link></li>,
+        <li><Link to="/services">Services</Link></li>,
+        <li><Link to="/blog">Blog</Link></li>,
+        <li><Link to="/contact">Contact</Link></li>
+]
+    return (
+        <div className="navbar bg-base-100 h-28
+        ">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact font-semibold dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        {navigation}
+      </ul>
+    </div>
+    <Link to='/'><img className='h-20' src={logo} alt="" /></Link>
+  </div>
+  <div className="navbar hidden lg:flex">
+    <ul className="menu menu-horizontal px-1 font-semibold">
+      {navigation}
+    </ul>
+  </div>
+  <div className="navbar-end">
+  <button className="btn btn-outline  btn-warning">Warning</button>
+  </div>
+</div>
+    );
+};
+
+export default Header;
